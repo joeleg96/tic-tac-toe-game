5c89468fa453361a48d13065a055cb3eac787db8
Joel Eguiza <joeleg96@gmail.com>
2025-06-11 19:32:30 -0600

    fixed javascript tile hover states, where it would still show the outline even if a tile had already been chosen.
    included victory banner html and css
    
    began coding cpu js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var playerOneMark = [];
 var playerTwoMark = [];
+var cpuMark = [];
 
 var playerOneMoves = [];
 var playerTwoMoves = [];
-var AvailableTiles = ["tile-1", "tile-2", "tile-3", "tile-4", "tile-5", "tile-6", "tile-7", "tile-8", "tile-9"];
+var cpuMoves = [];
+var availableTiles = ["tile-1", "tile-2", "tile-3", "tile-4", "tile-5", "tile-6", "tile-7", "tile-8", "tile-9"];
 
 
 // New Game Functions
@@ -13,10 +15,10 @@ $(".select-x").on("click", function(event) {
     $(".x-outline-silver").removeClass("hide");
     playerOneMark = [];
     playerTwoMark = [];
+    cpuMark = [];
     playerOneMark.push(this.id);
     playerTwoMark.push("o");
-    console.log("Player one: " + playerOneMark);
-    console.log("Player two: " + playerTwoMark);
+    cpuMark.push("o");
 
     $(".select-o").removeClass("selected");
     $(".o-outline").removeClass("hide");
@@ -29,10 +31,10 @@ $(".select-o").on("click", function() {
     $(".o-outline-silver").removeClass("hide");
     playerOneMark = [];
     playerTwoMark =[];
+    cpuMark = [];
     playerOneMark.push(this.id);
     playerTwoMark.push("x");
-    console.log("Player one: " + playerOneMark);
-    console.log("Player two: " + playerTwoMark);
+    cpuMark.push("x");
 
     $(".select-x").removeClass("selected");
     $(".x-outline").removeClass("hide");
@@ -42,7 +44,13 @@ $(".select-o").on("click", function() {
 $(".vs-cpu-button").on("click", function() {
     if (playerOneMark != "") {
         $(".new-game-flex-container").addClass("hide");
-    }
+        if (playerOneMark[0] == "x") {
+            playerOneTurn();
+        };
+         if (playerOneMark[0] == "o") {
+            setTimeout(cpuTurn, 2000);
+         }
+    };
 
 });
 
@@ -53,11 +61,53 @@ $(".vs-player-button").on("click", function() {
 });
 
 // Game Functions
-$(".game-tile").hover(function (event) {
+// $(".game-tile").hover(function (event) {
+//     var hoverMark = (playerOneMark + "-hover-" + this.id);
+//     if(availableTiles.indexOf(this.id) !== -1) {
+//         $("." + hoverMark).removeClass("hide"); 
+//     };
+    
+// }, 
+// function () {
+//     var hoverMark = (playerOneMark + "-hover-" + this.id);
+//     $("." + hoverMark).addClass("hide");
+    
+// });
+
+function playerOneTurn() {
+    $(".game-tile").on("click", function(event) {
+        console.log(this.id);
+        console.log(playerOneMark[0]);
+        $("." + playerOneMark +"-" + this.id).removeClass("hide");
+        playerOneMoves.push(this.id); 
+        availableTiles.splice(availableTiles.indexOf(this.id),1);
+        var hoverMark = (playerOneMark + "-hover-" + this.id);
+        $("." + hoverMark).addClass("hide");
+    });
+    
+    $(".game-tile").hover(function (event) {
     var hoverMark = (playerOneMark + "-hover-" + this.id);
-    $("." + hoverMark).removeClass("hide");
+    if(availableTiles.indexOf(this.id) !== -1) {
+        $("." + hoverMark).removeClass("hide"); 
+    };
+    
 }, 
 function () {
     var hoverMark = (playerOneMark + "-hover-" + this.id);
     $("." + hoverMark).addClass("hide");
+    
 });
+};
+
+function cpuTurn() {
+    var numberOfFreeTiles = Math.floor(availableTiles.length)-1;
+    var randomNumber = Math.floor(Math.random() * numberOfFreeTiles);
+    cpuChosenTile = ( "-tile-" + randomNumber);
+    $("." + cpuMark + cpuChosenTile).removeClass("hide");
+    cpuMoves.push(cpuChosenTile);
+    availableTiles.splice(availableTiles.indexOf(cpuChosenTile),1);
+    console.log(availableTiles);
+
+
+
+};
